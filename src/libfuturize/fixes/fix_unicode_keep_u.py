f3162959ae6442bebcd3cb36f3644aa6788578e5
Manish Singh <manish.singh@nutanix.com>
2020-10-08 06:59:18 +0000

    removing conversion of unicode to str as we have done it in our fixes

--- src/libfuturize/fixes/fix_unicode_keep_u.py
+++ src/libfuturize/fixes/fix_unicode_keep_u.py
@@ -11,11 +11,11 @@ of the porting task.
 from lib2to3.pgen2 import token
 from lib2to3 import fixer_base
 
-_mapping = {u"unichr" : u"chr", u"unicode" : u"str"}
+_mapping = {u"unichr" : u"chr"}
 
 class FixUnicodeKeepU(fixer_base.BaseFix):
     BM_compatible = True
-    PATTERN = "'unicode' | 'unichr'"
+    PATTERN = "'unichr'"
 
     def transform(self, node, results):
         if node.type == token.NAME:
